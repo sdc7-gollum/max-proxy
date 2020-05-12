3e205e0ea7b103bae632c4acfbda726a6121f88a
LeiLei Ros <leilei.ros+dev@gmail.com>
2020-05-12 02:44:14 -0400

    Add proxy middleware

--- server/server.js
+++ server/server.js
@@ -1,12 +1,22 @@
 /* eslint-disable no-console */
 require('dotenv').config();
 const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const host = process.env.SERVER_HOST;
 const port = process.env.SERVER_PORT;
-
-// Server Connection
 const app = express();
+
+// Middleware
+app.use(createProxyMiddleware(`http://${host}:9800/api/photos`, {
+  changeOrigin: true,
+  router: {
+    'http://localhost:9800': 'http://localhost:9000'
+  }
+}));
+app.use(express.static('public'));
+app.use(express.urlencoded());
+
 app.listen(process.env.SERVER_PORT, (err) => {
   if (err) {
     console.log('Error starting up server');
@@ -15,11 +25,9 @@ app.listen(process.env.SERVER_PORT, (err) => {
   }
 });
 
-app.use(express.static('public'));
-app.use(express.urlencoded());
 
 // HTTP Handlers
-// app.route('/:id')
-//   .get((req, res, next) {
-
-//   })
+app.get('/api/photos/:id', (req, res, next) => {
+  res.redirect(`http://${host}:9800/?${req.params.id}`);
+  next();
+});
